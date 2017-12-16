0680777092a8cca28f8c1f6e70c5c32679380dfe
PalmPTSJ <palm_bubui@hotmail.com>
2017-12-16 21:44:08 +0700

    Added index and sensor page

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.get('/sensor', (req, res) => {
+    // Example : sensor?value=1.0
+    console.log("Sensor value : "+req.query.value);
+    let sensorValue = req.query.value;
+    res.send('OK '+sensorValue);
+});
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'));

8702cce5d1ce28164403f28c8575a1767f3f8e90
PalmPTSJ <palm_bubui@hotmail.com>
2017-12-17 00:45:50 +0700

    Added basic webcam feature

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nodeWebcam = require( "node-webcam" );
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -8,9 +9,23 @@ app.get('/', (req, res) => {
 
 app.get('/sensor', (req, res) => {
     // Example : sensor?value=1.0
-    console.log("Sensor value : "+req.query.value);
     let sensorValue = req.query.value;
+    console.log("Sensor value : "+sensorValue);
     res.send('OK '+sensorValue);
+});
+
+var webcam_opts = {
+    width: 1280,
+    height: 720,
+    delay: 0,
+    callbackReturn: "base64",
+};
+app.get('/webcam', (req, res) => {
+    console.log("Webcam !")
+    nodeWebcam.capture("test_picture", webcam_opts, function( err, data ) {
+        console.log("Webcam captured !");
+        res.send('<html><body><img src="'+data+'"></body></html>');
+    });
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
